c5721a589c4c416e1068b42144a1aebe78a462ae
Vigilio <yonatanvigiliolavado09@gmail.com>
2022-01-06 05:48:04 -0500

    carousel style add . responsive

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
-var glider = new GLider(document.querySelector('.list'),{
+var glider = new Glider(document.querySelector('.list'), {
     slidesToShow: 1, // empieza como modo celular . muestra el tamaño de la imagen
     slidesToScroll: 1, // cuando le das siguiente los pasos que va dar
     draggable: true, // arrastable
     //dots: '.carousel__indicadores',  // indicador de abajo
     arrows: {
-        prev: '.carousel__anterior',  //boton anterior
-        next: '.carousel__siguiente' //boton siguiente
+        prev: '.contenedor-previous',  //boton anterior
+        next: '.contenedor-next' //boton siguiente
     },
-    responsive: [ //responsive
-        {
-          // screens greater than >= 775px
-          breakpoint: 450,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            duration: 0.5
-          }
-        },{
-          // screens greater than >= 1024px
-          breakpoint: 800,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            duration: 0.5,
-          }
-        }
-    ] 
+    
 });
+
+	// funcion automatico
+	function sliderAuto(slider, miliseconds) {
+		const slidesCount = slider.track.childElementCount;
+		let slideTimeout = null;
+		let nextIndex = 1;
+	   
+		function slide () {
+		  	slideTimeout = setTimeout(
+			function () {
+			  if (nextIndex >= slidesCount ) {
+				nextIndex = 0;
+			  }
+			  slider.scrollItem(nextIndex++);
+			},
+			miliseconds
+		  );
+		}
+	   
+		slider.ele.addEventListener('glider-animated', function() {
+		  window.clearInterval(slideTimeout);
+		  slide();
+		});
+	   
+		slide();
+	}
+	   sliderAuto(glider, 3000)
